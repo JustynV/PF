0834eef85feddd76e758c9b444f64954090eba78
Justyn <justynv@uninorte.edu.co>
2024-05-25 00:19:25 -0500

    added pages

---
+++ PF-FrontEnd/pages/innerAnalysis.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="EasyStock Análisis Interno",
+                   page_icon=":chart_with_upwards_trend:")
+st.title('EasyStock Analisis Interno')
+st.header('WIP (Coming soon)')
